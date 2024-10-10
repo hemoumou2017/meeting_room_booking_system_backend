755841d1e9eab984efaef388ad43405038b35cd4
何欣 <1254409474@qq.com>
2024-10-10 17:11:48 +0800

    预定管理--列表和修改状态

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2024-10-09 15:19:32
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-10-09 16:23:43
+ * @LastEditTime: 2024-10-10 16:50:45
  * @FilePath: /meeting_room_booking_system_backend/src/booking/booking.service.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -129,8 +129,8 @@ export class BookingService {
         bookingTimeRangeEnd = bookingTimeRangeStart + 60 * 60 * 1000;
       }
       condition.startTime = Between(
-        new Date(bookingTimeRangeStart),
-        new Date(bookingTimeRangeEnd),
+        new Date(+bookingTimeRangeStart),
+        new Date(+bookingTimeRangeEnd),
       );
     }
     const [bookings, totalCount] = await this.entityManager.findAndCount(
