49b88dd9dfe281b831b97e62eb44909e99ebd3d3
何欣 <1254409474@qq.com>
2024-10-11 10:20:35 +0800

    预定管理--预定缺失字段note

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2024-10-09 15:19:32
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-10-10 16:50:45
+ * @LastEditTime: 2024-10-11 10:19:28
  * @FilePath: /meeting_room_booking_system_backend/src/booking/booking.service.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -193,6 +193,7 @@ export class BookingService {
     booking.user = user;
     booking.startTime = new Date(bookingDto.startTime);
     booking.endTime = new Date(bookingDto.endTime);
+    booking.note = bookingDto.note;
 
     const res = await this.entityManager.findOneBy(Booking, {
       room: {
